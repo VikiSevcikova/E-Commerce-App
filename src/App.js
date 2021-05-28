abcaf3ccc8c506db3aaaf092d1f9b15383092b38
VikiSevcikova <viki.sevcikova63@gmail.com>
2021-05-28 11:27:05 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import Navbar from "./components/navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Footer/>
     </div>
   );
 }
