d9c106e068ccf2fd95b28144015f9c8a40570a5e
VikiSevcikova <viki.sevcikova63@gmail.com>
2021-05-28 16:12:59 -0700

    added section for products and created product card component

---
+++ src/components/productCard/ProductCard.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import product from '../../assets/product1.jpg';
+import {FaPlusSquare} from 'react-icons/fa';
+import './ProductCard.css';
+
+const ProductCard = () => {
+
+    return(
+        <div className="col-md-3">
+            <div class="card border-0" style={{width: "18rem"}} href="#">
+                <div className="product-image" >
+                    <a href="#">
+                        <img src={product} class="card-img-top rounded-0" alt="p1"/>
+                    </a>
+                    <div className="quick-add-overlay rounded py-2 px-2">
+                        <h6 className="text-uppercase fw-bold">Quick Add</h6>
+                        <button className="btn p-0" type="button"><FaPlusSquare/></button>
+                    </div>
+                </div>
+               
+                
+                <div class="card-body px-0 mx-0">
+                    <a href="#" class="card-text text-decoration-none link-dark d-flex flex-row justify-content-between">
+                        <h6>Title of the product</h6>
+                        <h6 className="fw-bold">$20.00 CAD</h6>
+                    </a>
+                </div>
+            </div>
+        </div>             
+     
+    );
+}
+
+export default ProductCard;
--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import banner from '../../assets/banner.jpg';
+import ProductCard from '../../components/productCard/ProductCard';
 
 const Home = () => {
 
@@ -13,6 +14,20 @@ const Home = () => {
                 <h1 className="display-5 fw-bold text-uppercase">Title</h1>
                 <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
                     <button type="button" className="btn btn-dark btn-lg px-4 text-uppercase">Shop</button>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="d-flex flex-row justify-content-between mt-5 mb-3">
+                    <h4 className="fw-bold text-uppercase">Category 1</h4>
+                    <a className="link-dark fw-bold" href="#">View all</a>
+                </div>
+         
+                <div className="row">
+                   <ProductCard/>
+                   <ProductCard/>
+                   <ProductCard/>
+                   <ProductCard/>
                 </div>
             </div>
         </div>
