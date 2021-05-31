abc5165f29d005852176ef3e994b5e0c9f29cddd
Viki <viki.sevcikova63@gmail.com>
2021-05-31 15:46:04 -0700

    changed homepage to react boostrap components, fixed padding in footer and navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
 
     return(
         <>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light px-2">
             <div className="container">
                 <a className="navbar-brand" href="#"><FaRegHandPeace/>Brand</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -4,85 +4,82 @@ import women from '../../assets/women.jpg';
 import men from '../../assets/men.jpg';
 import onsideimage from '../../assets/onsideimage.jpg';
 import ProductCard from '../../components/productCard/ProductCard';
+import { Container, Row, Col, Button, Image } from "react-bootstrap";
 
 const Home = () => {
 
     return(
-        <div className="container-fluid">
-            <div className="row">
-                <div className="p-2 d-flex flex-column justify-content-end justify-content-md-center align-items-center" 
+        <Container fluid>
+            <Row>
+                <Col className="d-flex flex-column justify-content-end justify-content-md-center align-items-center p-5" 
                     style={{
                         backgroundImage: `url(${banner})`,
                         backgroundSize: "cover",
                         height: "90vh"}}>
                     <h1 className="display-5 fw-bold text-uppercase">Title</h1>
-                    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                        <button type="button" className="btn btn-dark btn-lg px-4 text-uppercase">Shop</button>
-                    </div>
-                </div>
-            </div>
-           
+                    <Button variant="dark" size="lg" type="button" className="px-4 text-uppercase">Shop</Button>
+                </Col>
+            </Row>
 
-            <div className="container my-3">
+            <Container className="my-3">
                 <div className="d-flex flex-row justify-content-between mb-3">
                     <h4 className="fw-bold text-uppercase">Category 1</h4>
                     <a className="link-dark fw-bold" href="#">View all</a>
                 </div>
-         
-                <div className="row">
+                <Row>
                    <ProductCard/>
                    <ProductCard/>
                    <ProductCard/>
                    <ProductCard/>
-                </div>
-            </div>
+                </Row>
+            </Container>
 
-            <div className="p-2 d-flex flex-row" >
-                <div className="col-6">
-                    <img className="img-fluid" src={onsideimage}/>
-                </div>
-                <div className="col-6 d-flex flex-column justify-content-center align-items-center">
+            <Row className="p-2 align-items-center" >
+                <Col md={6}>
+                    <Image fluid src={onsideimage}/>
+                </Col>
+                <Col md={6} className="text-center">
                     <h1 className="display-5 fw-bold text-uppercase">Title</h1>
-                    <button type="button" className="btn btn-dark btn-lg px-4 text-uppercase">Shop</button>
-                </div>
-            </div>
+                    <Button variant="dark" size="lg" type="button" className="px-4 text-uppercase">Shop</Button>
+                </Col>
+            </Row>
 
-            <div className="container my-3">
+            <Container className="my-3">
                 <div className="d-flex flex-row justify-content-between mb-3">
                     <h4 className="fw-bold text-uppercase">Category 1</h4>
                     <a className="link-dark fw-bold" href="#">View all</a>
                 </div>
          
-                <div className="row">
+                <Row>
                    <ProductCard/>
                    <ProductCard/>
                    <ProductCard/>
                    <ProductCard/>
-                </div>
-            </div>
+                </Row>
+            </Container>
 
-            <div className="row p-2">
-                <div className="col-md-6">
-                    <div className="col-md-12 p-4 gap-2 d-flex flex-column justify-content-end justify-content-md-center align-items-center align-items-md-end" style={{
+            <Row className="p-2">
+                <Col md={6} className="mt-2">
+                    <Col md={12} className="d-flex flex-column justify-content-end align-items-end p-4 gap-2" style={{
                         backgroundImage: `url(${women})`,
                         backgroundSize: "cover",
                         height: "90vh"}}>
                         <h1 className="display-5 fw-bold text-uppercase text-light">Women</h1>
-                        <button type="button" className="btn btn-light btn-lg fw-bold px-4 text-uppercase">Shop</button>
-                    </div>
-                </div>
-                <div className="col-md-6">
-                    <div className="col-md-12 p-4 gap-2 d-flex flex-column justify-content-end justify-content-md-center align-items-center align-items-md-start" style={{
-                        backgroundImage: `url(${men})`,
-                        backgroundSize: "cover",
-                        height: "90vh"}}>
-                        <h1 className="display-5 fw-bold text-uppercase text-light">Men</h1>
-                        <button type="button" className="btn btn-light btn-lg fw-bold px-4 text-uppercase">Shop</button>
-                    </div>
-                </div>
-            </div>
-
-        </div>
+                        <Button variant="light" size="lg" type="button" className="fw-bold px-4 text-uppercase">Shop</Button>
+                    </Col>
+                </Col>
+                <Col md={6} className="mt-2">
+                        <Col md={12} className="text-start p-4 gap-2" style={{
+                            backgroundImage: `url(${men})`,
+                            backgroundSize: "cover",
+                            backgroundPosition: "center",
+                            height: "90vh"}}>
+                            <h1 className="display-5 fw-bold text-uppercase text-light">Men</h1>
+                            <Button variant="light" size="lg" type="button" className="fw-bold px-4 text-uppercase">Shop</Button>
+                        </Col>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
