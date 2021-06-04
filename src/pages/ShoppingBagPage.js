27579fe7b463eac08c904d2b58ba937750d46144
Ondrej Belza <belzaondra@gmail.com>
2021-06-04 15:26:13 -0700

    adds checkout

--- src/pages/ShoppingBagPage.js
+++ src/pages/ShoppingBagPage.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Col, Container, Row, Button } from "react-bootstrap";
 import ShoppingBagItem from "../components/ShoppingBagItem";
+import { loadStripe } from "@stripe/stripe-js";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+const stripePromise = loadStripe(
+  "pk_test_51IQ4BPHDh0apBsvmagmJGtI6rFMu5hr4Fjvm3HUKLigHcaN5nckdZhRMOBwoyQIMzsZEBJmmvv7ttw4i4sUFO6cg00tk66uFh1"
+);
 
 const ShoppingBag = ({ bag, setBag }) => {
   const [total, setTotal] = useState(0);
@@ -8,6 +14,35 @@ const ShoppingBag = ({ bag, setBag }) => {
   useEffect(() => {
     setTotal(bag.reduce((a, b) => a + b.price * b.quantity, 0));
   }, [bag]);
+
+  const checkout = async () => {
+    const stripe = await stripePromise;
+    const response = await axios.post(
+      "https://e-commerce-api.belzaondrej.com/create-checkout-session",
+      {
+        items: bag.map((p) => {
+          return {
+            price: p.price,
+            quantity: p.quantity,
+            image: p.previewImage,
+            name: `${p.name} - ${p.size}`,
+          };
+        }),
+      }
+    );
+
+    const session = response.data;
+
+    // When the customer clicks on the button, redirect them to Checkout.
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+    if (result.error) {
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, display the localized error message to your customer
+      // using `result.error.message`.
+    }
+  };
   return (
     <>
       <Container>
@@ -51,7 +86,13 @@ const ShoppingBag = ({ bag, setBag }) => {
             </Row>
             <Row className="d-flex justify-content-end my-3">
               <Col md lg={2}>
-                <Button variant="dark" className="w-100">
+                <Button
+                  variant="dark"
+                  className="w-100"
+                  onClick={() => {
+                    checkout();
+                  }}
+                >
                   Checkout
                 </Button>
               </Col>
